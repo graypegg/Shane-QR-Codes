d977cc1d211019e7d2cd56311fd55bd870007fff
Graham Pegg <toish@toi.sh>
2018-12-01 22:52:39 -0500

    BE - Set up a QR image endpoint at “GET //barcode”

--- api/web/endpoints/profile.js
+++ api/web/endpoints/profile.js
@@ -1,4 +1,6 @@
 const colours = require('../colours.json');
+var QRCode = require('qrcode')
+
 
 module.exports = (app, pool) => {
   app.get('/profile/:username', async (req, res) => {
@@ -19,6 +21,20 @@ module.exports = (app, pool) => {
             id: colourRow.colour
           }))
         });
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: 'Something went horribly wrong' });
+      }
+    } else {
+      res.status(400).json({ message: 'no colour param' });
+    }
+  })
+
+  app.get('/profile/:username/barcode.png', async (req, res) => {
+    if (req.params.username) {
+      try {
+        res.setHeader("Content-Type", "image/png");
+        QRCode.toFileStream(res, `http://toi.sh/hanky/profile/${req.params.username}`);
       } catch (e) {
         console.error(e);
         res.status(500).json({ message: 'Something went horribly wrong' });
