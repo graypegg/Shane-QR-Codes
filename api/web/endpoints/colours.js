996f1d07a7cb52fb9c11c1ecba7913d608d22957
Graham Pegg <toish@toi.sh>
2018-12-01 22:12:15 -0500

    BE - Enpoint to remove new colour at “DELETE /colours/”

--- api/web/endpoints/colours.js
+++ api/web/endpoints/colours.js
@@ -33,4 +33,30 @@ module.exports = (app, pool) => {
       }
     }
   )
+
+  app.delete('/colours/:colourId',
+    jwt({ secret: 'ailurus' }),
+    async (req, res) => {
+      if (req.params.colourId) {
+        try {
+          const { rowCount } = await pool.query(
+            `
+              DELETE FROM "colours" WHERE "colour"=$1 AND "user_id"=$2;
+            `, [
+              req.params.colourId,
+              req.user.id
+            ]
+          );
+          if (rowCount) res.json({ message: 'deleted.' });
+          else res.status(400).json({ message: 'that colour is not selected for this user' });
+        } catch (e) {
+          console.error(e);
+          if (e.code === '23505') res.status(400).json({ message: `colour #${req.body.colour} already selected for this user` });
+          else res.status(500).json({ message: 'Something went horribly wrong' });
+        }
+      } else {
+        res.status(400).json({ message: 'no colour param' });
+      }
+    }
+  )
 }
